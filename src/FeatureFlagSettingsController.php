2d1ede6577e0b872da64cdda72a090666882f6e0
Nathan Kirschbaum <nathan@nathankirschbaum.com>
2016-02-26 16:58:29 -0500

    enabling auth

--- src/FeatureFlagSettingsController.php
+++ src/FeatureFlagSettingsController.php
@@ -9,7 +9,7 @@ class FeatureFlagSettingsController extends Controller
 {
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function getSettings()
