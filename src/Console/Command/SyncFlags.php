991d4cb43e6b4f03bd9072cbbf15dc4017bfaad8
Damian Lee <damiankloip@gmail.com>
2018-06-14 16:49:27 +0100

    Register command, skip-cleanup, logging

--- src/Console/Command/SyncFlags.php
+++ src/Console/Command/SyncFlags.php
@@ -13,7 +13,7 @@ class SyncFlags extends Command {
      *
      * @var string
      */
-    protected $signature = 'feature-flag:sync {--force}';
+    protected $signature = 'feature-flag:sync {--force} {--skip-cleanup}';
 
 
     /**
@@ -40,23 +40,46 @@ class SyncFlags extends Command {
      */
     public function handle()
     {
+        if (!$this->option('force') && !$this->confirm('Are you sure?')) {
+            return;
+        }
+
         $default_flags = (array) $this->getLaravel()
             ->make('config')
             ->get('laravel-feature-flag.sync_flags', []);
 
         foreach ($default_flags as $key => $default_value) {
-            if (FeatureFlag::whereKey($key)->exists()) {
+            if (FeatureFlag::where('key', $key)->exists()) {
                 $this->printAndLogLine(sprintf('Feature flag with key "%s" already exists', $key));
                 continue;
             }
 
             // Otherwise, create the new flag with default value.
-            $feature = new FeatureFlag();
-            $feature->key = $key;
-            $feature->variants = $default_value;
-            $feature->save();
+            $flag = $this->createFeatureFlag($key, $default_value);
 
-            $this->printAndLogInfo(sprintf('Feature flag created with key "%s" with variant "%s"', $key, json_encode($default_value)));
+            $this->printAndLogInfo(sprintf('Feature flag created with key "%s" [%d] with variant "%s"', $key, $flag->id, json_encode($default_value)));
+        }
+
+        if ($this->option('skip-cleanup')) {
+            return;
+        }
+
+        // Tidy up and flags that are not present any more.
+        $existing_keys = array_keys($default_flags);
+
+        $query = FeatureFlag::whereNotIn('key', $existing_keys);
+
+        if ($query->exists()) {
+            $this->printAndLogLine(sprintf('Removing flags not defined in sync_flags'));
+
+            $query->chunk(100, function($chunk) {
+                foreach ($chunk as $flag) {
+                    $flag->delete();
+                    $this->printAndLogLine(sprintf('Feature flag with key "%s" deleted', $flag->key));
+                }
+            });
+        } else {
+            $this->printAndLogLine(sprintf('No flags to remove'));
         }
     }
 
@@ -70,5 +93,15 @@ class SyncFlags extends Command {
     {
         $this->info($line);
         $this->logger->info($line);
+    }
+
+    private function createFeatureFlag($key, $value)
+    {
+        $flag = new FeatureFlag();
+        $flag->key = $key;
+        $flag->variants = $value;
+        $flag->save();
+
+        return $flag;
     }
 }
